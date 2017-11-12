6d6dc7ef9cdc6c9455237cdd03f57b4b8b28979c
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-12 15:06:56 +0300

    Add dynamic content. Update urls in tribune

--- tribune/news/urls.py
+++ tribune/news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url( r'^$', views.welcome, name="welcome"),
+    url( r'^today/$', views.news_of_day, name="newsToday")
 ]
 
--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,6 +1,51 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
+    '''
+    View function for the root page
+    '''
     return HttpResponse('Welcome to Moringa Tribune')
+
+def news_of_day(request):
+    '''
+    View function for the news of the current day
+    '''
+    date = dt.date.today()
+
+    # Convert date object to find exact day
+    day = convert_dates(date)
+
+    html = f'''
+        <html>
+            <body>
+                <h1> News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+    '''
+    Function that takes a date object and returns the day of the week
+
+    Args:
+        date : date object
+
+    Returns:
+        day : day of the week
+    '''
+    # Get week day number from the date
+    day_number = dt.date.weekday(dates)
+
+    # List of days of the week
+    days = ['Monday', 'Tuesday', 'Wednewday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    # Actual day of the week to be returned
+    day = days[day_number]
+
+    return day
+
+
