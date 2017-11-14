4fc1091c7fb906bb7b84f72ef0f1300be61b93a3
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-14 11:15:15 +0300

    Create Editor model. Migrate to database with Editor model

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    '''
+    Class that defines an Editor for an article
+    '''
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        # return ' %s' %  (self.first_name)
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
