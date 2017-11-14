e26722e2975d3d775b646461617b643045d5da38
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-14 19:01:15 +0300

    Create column for an article. Upgrade to article table with image for an article

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -83,6 +83,7 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    article_image = models.ImageField(upload_to='articles/', null=True)
 
     def __str__(self):
         return self.title
