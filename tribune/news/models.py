ab146f82181390e266e0ee33dbf8c247fe819a2a
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-14 14:35:44 +0300

    Create Django admin account. Connect model to /admin route. Update editor table to have phone number column. Migrate to editor table with optional phone number column

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -8,6 +8,7 @@ class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
         # return ' %s' %  (self.first_name)
@@ -78,7 +79,7 @@ class Article(models.Model):
     '''
     title = models.CharField(max_length=60)
     post = models.TextField()
-    editor = models.ForeignKey(Editor)
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True, null=True)
 
