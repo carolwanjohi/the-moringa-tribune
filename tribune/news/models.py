50a6a1c5d80b4e9237c11925f113fb8016b1cc22
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-14 13:49:08 +0300

    Create article and tags classes. Migrate to latest database with publication date. Create test for instance, savem delete and update class methods for Editor, Article and tags

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -15,3 +15,97 @@ class Editor(models.Model):
 
     class Meta:
         ordering = ['first_name']
+
+    def save_editor(self):
+        '''
+        Method to save a new Editor to the database
+        '''
+        self.save()
+
+    def delete_editor(self):
+        '''
+        Method to delete an Editor from the database
+        '''
+        self.delete()
+
+    @classmethod
+    def get_editors(cls):
+        '''
+        Method that gets all editors from the database
+
+        Returns:
+            editors : list of editor objects from the database
+        '''
+        editors = Editor.objects.all()
+        return editors
+
+
+class tags(models.Model):
+    '''
+    Class that defines Tags for an Article
+    '''
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_tag(self):
+        '''
+        Method to save a new tag to the database
+        '''
+        self.save()
+
+    def delete_tag(self):
+        '''
+        Method to delete a tag from the database
+        '''
+        self.delete()
+
+    @classmethod
+    def get_tags(cls):
+        '''
+        Method that gets all tags from the database
+
+        Returns:
+            gotten_tags : list of tag objects from the database
+        '''
+        gotten_tags = tags.objects.all()
+        return gotten_tags
+
+class Article(models.Model):
+    '''
+    Class that defines an Article
+    '''
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_article(self):
+        '''
+        Method to save a new Article to the database
+        '''
+        self.save()
+
+    def delete_article(self):
+        '''
+        Method to delete an Article from the database
+        '''
+        self.delete()
+
+    @classmethod
+    def get_articles(cls):
+        '''
+        Method that gets all articles from the database
+
+        Returns:
+            articles : list of article objects from the database
+        '''
+        articles = Article.objects.all()
+        return articles
+
+
--- tribune/news/tests.py
+++ tribune/news/tests.py
@@ -1,3 +1,142 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+    '''
+    Test case for Editor class
+    '''
+
+    # Set Up method
+    def setUp(self):
+        '''
+        Method that sets up an Editor instance before each test
+        '''
+        self.new_editor = Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_editor in an instance of Editor class
+        '''
+        self.assertTrue( isinstance(self.new_editor, Editor) )
+
+    def test_save_editor(self):
+        '''
+        Test case to check is an editor is saved in the database
+        '''
+        self.new_editor.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue( len(editors) > 0 )
+
+    def test_delete_editor(self):
+        '''
+        Test case to check if an editor is deleted from the database
+        '''
+        self.new_editor.save_editor()
+        editors = Editor.objects.all()
+        self.new_editor.delete_editor()
+        self.assertTrue( len(editors) == 0)
+
+    def test_get_editors(self):
+        '''
+        Test case to check if all editors are gotten from the database
+        '''
+        self.new_editor.save_editor()
+        gotten_editors = Editor.get_editors()
+        editors = Editor.objects.all()
+        self.assertTrue( len(gotten_editors) == len(editors))
+
+class ArticleTestClass(TestCase):
+    '''
+    Test case for Article class
+    '''
+
+    # Set Up method
+    def setUp(self):
+        '''
+        Method that sets up an Article instance before each test
+        '''
+        self.james = Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
+
+        self.new_article = Article(title = 'Python James', post ='Python James is Muriuki who wrote Python content for Moringa School', editor=self.james)
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_article in an instance of Article class
+        '''
+        self.assertTrue( isinstance(self.new_article, Article) )
+
+    def test_save_article(self):
+        '''
+        Test case to check is an Article is saved in the database
+        '''
+        self.james.save_editor()
+        self.new_article.save_article()
+        artilces = Article.objects.all()
+        self.assertTrue( len(artilces) > 0 )
+
+    def test_delete_article(self):
+        '''
+        Test case to check if an article is deleted from the database
+        '''
+        self.new_article.save_article()
+        artilces = Article.objects.all()
+        self.new_article.delete_article()
+        self.assertTrue( len(artilces) == 0 )
+
+    def test_get_articles(self):
+        '''
+        Test case to check if all articles are gotten from the database
+        '''
+        self.new_article.save_article()
+        gotten_articles = Article.get_articles()
+        artilces = Article.objects.all()
+        self.assertTrue( len(gotten_articles) == len(artilces))
+
+class tagsTestClass(TestCase):
+    '''
+    Test case for tags class
+    '''
+
+    # Set Up method
+    def setUp(self):
+        '''
+        Method that sets up a tags instance before each test
+        '''
+        self.new_tag = tags(name='Python')
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_tag in an instance of tags class
+        '''
+        self.assertTrue( isinstance(self.new_tag, tags) )
+
+    def test_save_editor(self):
+        '''
+        Test case to check is a tag is saved in the database
+        '''
+        self.new_tag.save_tag()
+        gotten_tags = tags.objects.all()
+        self.assertTrue( len(gotten_tags) > 0 )
+
+    def test_delete_tag(self):
+        '''
+        Test case to check if a tag is deleted from the database
+        '''
+        self.new_tag.save_tag()
+        gotten_tags = tags.objects.all()
+        self.new_tag.delete_tag()
+        self.assertTrue( len(gotten_tags) == 0 )
+
+    def test_get_tags(self):
+        '''
+        Test case to check if all tags are gotten from the database
+        '''
+        self.new_tag.save_tag()
+        gotten_tags = tags.get_tags()
+        existing_tags = tags.objects.all()
+        self.assertTrue( len(gotten_tags) == len(existing_tags))
+
+
+
