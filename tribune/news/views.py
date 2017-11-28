6d670679a3bc19e8aa7db069d45603a99cf4e639
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-28 14:15:43 +0300

    Create IsAdminOrReadOnly permission class to allow inly the admin privilages that other users do not have. Import IsAdminOrReadOnly class to views and create attribute permission_classes and pass in IsAdminOrReadOnly permission class

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import MerchSerializer
+from .permissions import IsAdminOrReadOnly
 
 # Create view functions here
 def news_today(request):
@@ -152,6 +153,8 @@ class MerchList(APIView):
     '''
     API view to handle requests
     '''
+    permission_classes = (IsAdminOrReadOnly,)
+
     def get(self, request, format=None):
         '''
         Get method where we query the database to get all MoringaMerch objects, serialize the data and return the serialized data as the response
